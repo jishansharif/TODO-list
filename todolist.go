70189258618b362a0b5f49b5c356c1c724d41cee
Jishan Sharif <jishanmsharif@gmail.com>
2020-02-02 11:17:15 -0500

    Renamed the file

--- todolist.go
+++ todolist.go
@@ -4,14 +4,13 @@ import "fmt"
 
 
 const Name = "Jishan"
-func todolist() {
+func main (){ 
+	func todolist() {
 	fmt.Println("Hello", Name , "What would you like to do today?")
 	var Title str = "Today's objectives"
 	var Note str = "Add Description"
 	var Deadline str = "Complete objective by date .. "
 	fmt.Println(Title,\n, Note, \n, Deadline \n)
-
-
+	}
 }
-
 
