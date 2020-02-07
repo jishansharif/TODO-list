2bc38cfd3da39e48bf4e3b1bb8ee00b773df964c
Jishan Sharif <jishanmsharif@gmail.com>
2020-02-06 22:08:43 -0500

    Implemented newtype by adding new variables

--- todolist.go
+++ todolist.go
@@ -12,5 +12,13 @@ func main() {
 	date := "06/02/2020"
 	Reminder := "Complete Math's Assignment"
 
+	secondreminder := firstreminder
+	secondreminder.date = "08/02/2020"
+	secondreminder.Reminder = "Complete Assignment Three for CS"
+
+	thirdreminder := firstreminder
+	thirdreminder.date = "09/02/2020"
+	thirdreminder.Reminder = "Study for Math's midterm!"
+
 	fmt.Printf("%s: %s\n", date, Reminder)
 }
