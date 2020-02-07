ce977a6281643a25480a1479c782d1dcb393c037
Jishan Sharif <jishanmsharif@gmail.com>
2020-02-06 22:48:09 -0500

    Added a new function

--- todolist.go
+++ todolist.go
@@ -1,25 +1,32 @@
 package main
 
-import "fmt"
-
 type todolist struct {
 	date     string
 	Reminder string
 	Priority int
 }
 
+func greaterimportance(first, second, todolist) todolist {
+	if first.Priority < second.Priority {
+		return first
+	}
+	return second
+}
+
 func main() {
 	firstreminder := todolist
 	date := "06/02/2020"
 	Reminder := "Complete Math's Assignment"
+	Priority := 2
 
 	secondreminder := firstreminder
 	secondreminder.date = "08/02/2020"
 	secondreminder.Reminder = "Complete Assignment Three for CS"
+	secondreminder.Priority = 3
 
 	thirdreminder := firstreminder
 	thirdreminder.date = "09/02/2020"
 	thirdreminder.Reminder = "Study for Math's midterm!"
+	thirdreminder.Priority = 1
 
-	fmt.Printf("%s: %s\n", date, Reminder)
 }
